ed9032f332b9be916852643a291d67f6d6ad6600
Administrator <892895953@qq.com>
2020-05-08 11:40:08 +0800

    接口测试学习笔记

---
+++ test_requests/test_wework/testcase/test_department.py
@@ -0,0 +1,31 @@
+from test_requests.test_wework.api.wework import WeWork
+from test_requests.test_zuoye.api.department import Department
+
+
+
+class TestDeparement:
+    def setup(self):
+        self.department = Department()
+
+    def test_token(self):
+        r = WeWork.get_token("aEOKRsyRLO4H8ETeLk2RawUW8IzAIAl2jW6N9T0TcPk")
+        assert r["errcode"] == 0
+
+    def test_create(self):
+        r = self.department.create("may", 1)
+        assert r["errcode"] == 0
+
+    def test_update(self):
+        r = self.department.update(3, name="abcd")
+        assert r["errmsg"] == "updated"
+
+    def test_delete(self):
+        r = self.department.delete(3)
+        assert r["errmsg"] == "deleted"
+
+    def test_get(self):
+        r = self.department.get(3)
+        print(r)
+        assert r["errmsg"] == "ok"
+
+
